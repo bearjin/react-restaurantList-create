8036e75e398ca83dd97ee959a6ecba0fa898342a
kwj5030 <30855594+kwj5030@users.noreply.github.com>
2021-04-19 22:10:29 +0900

    sub.css 파일 scss 로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './sub.css';
+import '.styles/styles';
 import LunchBox from './component/LunchBox';
 import RandomMenu from './component/RandomMenu';
 import Map from './component/Map';
@@ -6,10 +6,10 @@ import Map from './component/Map';
 function App() {
   return (
     <div className="App">
-        <p className="main_title">Uid 점심 메뉴 리스트</p>
-        <LunchBox></LunchBox>
-        <RandomMenu></RandomMenu>
-        <Map></Map>
+      <p className="main_title">Uid 점심 메뉴 리스트</p>
+      <LunchBox></LunchBox>
+      <RandomMenu></RandomMenu>
+      <Map></Map>
     </div>
   );
 }
